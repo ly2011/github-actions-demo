8e8730dc52fffd2331d66be10e7997812d5df992
ly2011 <1518550424@qq.com>
2019-09-25 09:08:56 +0800

    feat: update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 100 day
         </a>
       </header>
     </div>
